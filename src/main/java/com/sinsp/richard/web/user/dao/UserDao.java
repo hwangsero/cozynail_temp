12dc918cc0a5b7567a9bd3db07ca10b07a5cb547
Y.jiseon <yoosijisun12@gmail.com>
2020-05-11 00:46:32 +0900

    hello world!

--- src/main/java/com/sinsp/richard/web/user/dao/UserDao.java
+++ src/main/java/com/sinsp/richard/web/user/dao/UserDao.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 public class UserDao {
 
 	public void getUserList() {
-
+		System.out.println(">>>>>>>>>>>>>>> sql sql");
 	}
 
 }
--- src/main/java/com/sinsp/richard/web/user/service/UserService.java
+++ src/main/java/com/sinsp/richard/web/user/service/UserService.java
@@ -1,8 +1,5 @@
 package com.sinsp.richard.web.user.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface UserService {
 
 	public void getUserList();
