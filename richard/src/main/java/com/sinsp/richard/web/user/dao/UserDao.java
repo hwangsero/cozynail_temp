2208ad0eeeb5ced7108c9c9cc6262ef37a5a297a
Y.jiseon <yoosijisun12@gmail.com>
2020-05-11 20:27:34 +0900

    richard

---
+++ richard/src/main/java/com/sinsp/richard/web/user/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.sinsp.richard.web.user.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository //이부분에 선언해야한다고 메모해놓고 선언 안하는 실수를 바로 함;;;;;;;;
+public class UserDao {
+
+	public void getUserList() {
+		System.out.println(">>>>>>>>>>>>>>> sql sql");
+	}
+
+}
